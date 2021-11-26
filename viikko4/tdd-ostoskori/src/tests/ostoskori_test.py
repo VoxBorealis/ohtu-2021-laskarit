fee12c0c8c289e78bfab141a503f011e6a930546
VoxBorealis <uusitalo.y@gmail.com>
2021-11-26 17:38:53 +0200

    Add test for ostoskori

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -37,3 +37,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(banaani)
 
         self.assertEqual(self.kori.hinta(), 5)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_kaksi_tavara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)

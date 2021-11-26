3f61c36d6e73b494164bf29c4638693c73c5d650
VoxBorealis <uusitalo.y@gmail.com>
2021-11-26 18:18:54 +0200

    Use Dict instead of List for kori

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -1,30 +1,38 @@
+from typing import Dict
 from tuote import Tuote
 from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self._kori = []
+        #self._kori = []
+        self._kori = {}
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
         count = 0
-        for tuote in self._kori:
+        for tuote in self._kori.values():
             count += tuote.lukumaara()
         return count
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
         hinta = 0
-        for tuote in self._kori:
+        for tuote in self._kori.values():
             hinta += tuote.hinta()
         return hinta
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        self._kori.append(Ostos(lisattava))
+        #if self._kori.
+        #self._kori.append(Ostos(lisattava))
+        # hash
+        if not self._kori.get(lisattava.nimi):
+            self._kori[lisattava.nimi] = Ostos(lisattava)
+        else:
+            self._kori[lisattava.nimi].muuta_lukumaaraa(1)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -37,4 +45,4 @@ class Ostoskori:
     def ostokset(self):
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
-        return self._kori
+        return list(self._kori.values())
